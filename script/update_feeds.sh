cbc59eaf8959dc59eb1cf5321f317ed203808b52
dennis <47095866+Pooua@users.noreply.github.com>
2026-01-06 23:50:45 +0800

    Update and rename diy-part1.sh to script/update_feeds.sh

--- script/update_feeds.sh
+++ script/update_feeds.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 # Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
